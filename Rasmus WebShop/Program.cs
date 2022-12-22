a1b64725f4acedbffb1d976170ad3596e6d091a6
Ondastsaft <kenneth.jansson@gmail.com>
2022-12-22 09:24:13 +0100

    Init

---
+++ Rasmus WebShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rasmus_WebShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
